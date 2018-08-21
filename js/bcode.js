11f8f8954097efe5019f8613a81ed827838170b8
Aleksandr <larenkosha@gmail.com>
2018-08-21 18:10:23 +0300

    add blocks

--- js/bcode.js
+++ js/bcode.js
@@ -3,5 +3,11 @@ $(window).on('load', function(){
     $('body').fadeTo('slow' , 1);
 });
 $(document).ready(function() {
-    
+    $('.clients-carousel').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
 });
