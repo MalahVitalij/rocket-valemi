c6fddc0102177e5cd83c7ec84a8d3c85792ed468
Aleksandr <larenkosha@gmail.com>
2018-08-26 16:56:37 +0300

    app2

--- js/bcode.js
+++ js/bcode.js
@@ -10,4 +10,31 @@ $(document).ready(function() {
         slidesToShow: 3,
         slidesToScroll: 1,
     });
+    $('.popular-slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+    
+    $('.check-ball-wrapper .ball').on('click', function(){
+        $(this).find('img').attr('alt');//находим тег img, забираем атрибут alt
+        if (!$('.checked-ball .desc').text()) {//проверяем поле, если оно не пустое
+            $('.checked-ball .desc').text();//очищаем поле
+            $('.checked-ball .desc').text($(this).find('img').attr('alt'));//вставляем значение из найденого атрибута
+        } else {//если пустое 
+            $('.checked-ball .desc').text($(this).find('img').attr('alt'));//вставляем значение из найденого атрибута
+        }
+        
+        $(this).find('img').attr('src');
+        if (!$('.checked-ball .img-block').find('img').attr('src', '')) {
+            $('.checked-ball .img-block').find('img').attr('src', '');
+            $('.checked-ball .img-block').find('img').attr('src', $(this).find('img').attr('src'));
+            
+        } else {
+            $('.checked-ball .img-block').find('img').attr('src', $(this).find('img').attr('src'));
+        }
+    });
 });
